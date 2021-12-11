48e649574c774d1bb17e3e1757aa4fabbb111c64
Christian Dionisio <christiandionisio9432@gmail.com>
2021-12-10 19:59:40 -0500

    mock controller done

--- src/main/java/com/example/prospectomock/controllers/MockController.java
+++ src/main/java/com/example/prospectomock/controllers/MockController.java
@@ -1,19 +1,62 @@
 package com.example.prospectomock.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/mockService")
 public class MockController {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(MockController.class);
+
+    @Value("classpath:output.json")
+    Resource resourceFileResponseOK;
+
+
+    @GetMapping("/checkstatus")
+    public String checkStatus(){
+        return "STATUS OK";
+    }
+
     @PostMapping
-    public ResponseEntity<String> mockService(@RequestBody String input) {
-        return new ResponseEntity<String>("Hola mundo", HttpStatus.OK);
+    public ResponseEntity<Object> mockService(@RequestBody String input) {
+
+        Map<String, Object> mapResponse = resourceToJson(resourceFileResponseOK);
+
+        if (mapResponse == null) {
+            return new ResponseEntity<>("Error interno", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<Object>(mapResponse, HttpStatus.OK);
+    }
+
+    private Map<String, Object> resourceToJson(Resource resourcePath) {
+        try {
+            File file =  resourceFileResponseOK.getFile();
+            String json = new String(Files.readAllBytes(file.toPath()));
+            LOGGER.info("Json from file: " + json );
+
+            Map<String,Object> result = new ObjectMapper().readValue(json, HashMap.class);
+
+            return result;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOGGER.info("ERROR: " + e.getMessage());
+            return null;
+        }
     }
 
 }
